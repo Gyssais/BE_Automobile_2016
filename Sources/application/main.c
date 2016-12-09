538a4d44f8735f53924d85c80cd855289a13e009
GidoVW <gido_vanwijk@hotmail.com>
2016-12-09 15:13:53 +0100

    Fonctionnement CLK du SPI, manque MOSI

--- Sources/application/main.c
+++ Sources/application/main.c
@@ -16,53 +16,16 @@
 
 /*************** Public function            **********/
 
-// configure the adc, configure the pin AD_PIN for analog input, and perform continuously a software triggered conversion.
-void adc_example()
-{
-	int result;
-	
-	result = setupAdc();
-	result =  setupPin(AD_PIN);
-	enableADC();
-	
-	while(1)
-	{
-		result = analogRead(AD_PIN);
-	}
-}
-
- int main(void) {
+int main(void) {
 
 	 
+	
 	 disableWatchdog();
 	 initModesAndClock();
+	 initialise_SPI_DRIVER();
 	 
-	 /*
-	 SIU.PCR[68].R = 0x0200;// LED EN Sortie
-	 SIU.PCR[69].R = 0x0200;
-	 SIU.PCR[70].R = 0x0200;
-	 SIU.PCR[65].R = 0x0100; // S2 en entree
-	 SIU.PCR[64].R = 0x0100;
-	 SIU.PCR[66].R = 0x0100;
-	 while(1) {
-		 if(SIU.GPDI[65].B.PDI == 1) {
-			SIU.GPDO[68].R = 1; 
-		 }
-		 else SIU.GPDO[68].R = 0;
-		 
-		 if(SIU.GPDI[66].B.PDI == 1){
-			 SIU.GPDO[70].R= 1;}
-		 else SIU.GPDO[70].R = 0;
-		 
-		 if(SIU.GPDI[64].B.PDI == 1)
-			 
-		 {   SIU.GPDO[69].R = 1;
-		 }
-			else SIU.GPDO[69].R = 0;
-		
-	 }   */
-
-	 adc_example(); // To test
+	 SPI[1].init(SPI_BAUD_62500, SPI_DELAY_DEFAULT);
+	 Init_SBC_DBG();
 
 }
 	 
