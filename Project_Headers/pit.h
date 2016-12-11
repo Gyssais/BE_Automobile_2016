d6aaf3fc2808ef082ce0c8be7933e699ae9f4bcf
The00 <leurent@etud.insa-toulouse.fr>
2016-12-11 18:13:53 +0100

    add pit.h

---
+++ Project_Headers/pit.h
@@ -0,0 +1,18 @@
+/*
+ * pit.h
+ *
+ *  Created on: Dec 11, 2016
+ *      Author: Theo
+ */
+
+#ifndef PIT_H_
+#define PIT_H_
+
+
+#define F_PIT 10000000  // Hz
+
+void setupChannelPIT(unsigned int channel, unsigned int period); // setup the corresponding channel to generate an interrupt every period
+void startChannelPIT(unsigned int channel);
+void stopChannelPIT(unsigned int channel);
+
+#endif /* PIT_H_ */
