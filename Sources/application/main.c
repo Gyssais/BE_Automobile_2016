41f336cb115dbf4a6267d2645c2e549eedfb8029
Gyssais <julien.chatelais@free.fr>
2016-12-13 09:52:14 +0100

    Tests ok

--- Sources/application/main.c
+++ Sources/application/main.c
@@ -18,59 +18,13 @@
 
 /*************** Public function            **********/
 
-// configure the adc, configure the pin AD_PIN for analog input, and perform continuously a software triggered conversion.
+
 
 
  int main(void) {
-	 disableWatchdog();
-	 initModesAndClock();
 	 
-	 init_LED();
-	 init_buttons();
-	 
-	 while(1) {
-		 if(lock_door() == 1) {
-			LED_on(1); 
-		 }
-		 else LED_off(1);
-		 
-		 if(det_rain() == 1){
-			 LED_on(2);
-		 }
-		 else LED_off(2);
-		 
-		 if(bat_min() == 1) {   
-			 LED_on(3);
-		 }
-		 else LED_off(3);
-	 }
-	
-	
-	 /*
-	     SIU.PCR[68].R = 0x0200;// LED EN Sortie
-	 	 SIU.PCR[69].R = 0x0200;
-	 	 SIU.PCR[70].R = 0x0200;
-	 	 SIU.PCR[65].R = 0x0100; // S2 en entree
-	 	 SIU.PCR[64].R = 0x0100;
-	 	 SIU.PCR[66].R = 0x0100;
-	 	 while(1) {
-	 		 if(SIU.GPDI[65].B.PDI == 1) {
-	 			SIU.GPDO[68].R = 1; 
-	 		 }
-	 		 else SIU.GPDO[68].R = 0;
-	 		 
-	 		 if(SIU.GPDI[66].B.PDI == 1){
-	 			 SIU.GPDO[70].R= 1;}
-	 		 else SIU.GPDO[70].R = 0;
-	 		 
-	 		 if(SIU.GPDI[64].B.PDI == 1)
-	 			 
-	 		 {   SIU.GPDO[69].R = 1;
-	 		 }
-	 			else SIU.GPDO[69].R = 0;
-	 		
-	 	 }*/
-
+	disableWatchdog();
+	initModesAndClock();
 	
 }
 	 
