97811138d788f741e0a86f1c9a8e8233ef94f3d0
Gyssais <julien.chatelais@free.fr>
2016-12-16 10:12:55 +0100

    Problème compilation

--- Sources/application/main.c
+++ Sources/application/main.c
@@ -23,8 +23,8 @@
 	 
 	disableWatchdog();
 	initModesAndClock();
-  initialise_SPI_DRIVER();
-  SPI[1].init(SPI_BAUD_62500, SPI_DELAY_DEFAULT);
+	initialise_SPI_DRIVER();
+	SPI[1].init(SPI_BAUD_62500, SPI_DELAY_DEFAULT);
 	Init_SBC_DBG();
 	
 }
