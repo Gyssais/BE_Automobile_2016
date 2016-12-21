af7451eeb8a5d121545764526a6736a62b91a344
The00 <leurent@etud.insa-toulouse.fr>
2016-12-21 12:58:55 +0100

    add current_monitoring .c and .h

--- Sources/application/main.c
+++ Sources/application/main.c
@@ -18,62 +18,7 @@
 /*************** Public function            **********/
 
 // configure the adc, configure the pin AD_PIN for analog input, and perform continuously a software triggered conversion.
-void adc_example()
-{
-	int result;
-	
-	result = setupADC();
-	result =  setupPin_ADC(AD_PIN);
-	enableADC();
-	
-	while(1)
-	{
-		result = analogRead(AD_PIN);
-	}
-}
 
-
-void adc_wtch_isr()
-{
-	unsigned long flag = ADC.WTISR.R;
-}
-
-
-// configure PIT, CTU and ADC to trigger a conversion on AD_PIN every 100ms
-void ctu_trigger_example()
-{
-	int result;
-		
-	result = setupADC();
-	result = setupPin_ADC(AD_PIN);
-	
-	setupChannel_CTU_trigger(0); 	// link the PIT to the ADC channel 0 (PB4) through the CTU
-	setupChannelPIT(3, 100);  // use PIT_3, the only one to be linked to the CTU. period =100ms.
-	startChannelPIT(3);
-	
-	enableADC();
-	
-}
-
-void adc_watchdog_example()
-{
-	int result;
-		
-	result = setupADC();
-	result =  setupPin_ADC(AD_PIN);
-	
-	result = setupAnalogWatchdog(AD_PIN, 700, 100, 0); // high_treshold =700, low = 100, first watchdog (0)
-	attachInterrupt_ADC_WTCH(adc_wtch_isr, 7);
-	startAnalogWatchdog(0);
-	
-	enableADC();
-	
-	
-	while(1)
-		{
-			result = analogRead(AD_PIN);
-		}
-}
 
 
  int main(void) {
@@ -82,33 +27,6 @@ void adc_watchdog_example()
 	 disableWatchdog();
 	 initModesAndClock();
 	 
-	 /*
-	 SIU.PCR[68].R = 0x0200;// LED EN Sortie
-	 SIU.PCR[69].R = 0x0200;
-	 SIU.PCR[70].R = 0x0200;
-	 SIU.PCR[65].R = 0x0100; // S2 en entree
-	 SIU.PCR[64].R = 0x0100;
-	 SIU.PCR[66].R = 0x0100;
-	 while(1) {
-		 if(SIU.GPDI[65].B.PDI == 1) {
-			SIU.GPDO[68].R = 1; 
-		 }
-		 else SIU.GPDO[68].R = 0;
-		 
-		 if(SIU.GPDI[66].B.PDI == 1){
-			 SIU.GPDO[70].R= 1;}
-		 else SIU.GPDO[70].R = 0;
-		 
-		 if(SIU.GPDI[64].B.PDI == 1)
-			 
-		 {   SIU.GPDO[69].R = 1;
-		 }
-			else SIU.GPDO[69].R = 0;
-		
-	 }   */
-
-	// adc_example(); // To test
-	 adc_watchdog_example();
 }
 	 
 				
