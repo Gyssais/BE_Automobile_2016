6ace931868aceca984422c96216a4672e19c39cb
The00 <leurent@etud.insa-toulouse.fr>
2016-12-21 16:28:02 +0100

    finish ADC CTU triggered example

--- Sources/application/current_monitoring.c
+++ Sources/application/current_monitoring.c
@@ -6,6 +6,8 @@
  */
 
 #include "current_monitoring.h"
+#include "Buttons_management.h"
+
 
 void adc_example()
 {
@@ -27,21 +29,31 @@ void adc_wtch_isr()
 	unsigned long flag = ADC.WTISR.R;
 }
 
-
+void adc_eoc_isr()
+{
+	//toggle LED_1
+	if(SIU.GPDO[PE_4].R == 1) SIU.GPDO[PE_4].R = 0;
+	else SIU.GPDO[PE_4].R = 1;
+	
+}
 // configure PIT, CTU and ADC to trigger a conversion on AD_PIN every 100ms
 void ctu_trigger_example()
 {
 	int result;
-		
+	init_LED();
+	
 	result = setupADC();
 	result = setupPin_ADC(AD_PIN);
 	
-	setupChannel_CTU_trigger(0); 	// link the PIT to the ADC channel 0 (PB4) through the CTU
-	setupChannelPIT(3, 100);  // use PIT_3, the only one to be linked to the CTU. period =100ms.
-	startChannelPIT(3);
+	setupPin_ADC_Interrupt(PB_4, EOCTU_MASK);	// enable EOC_CTU interrupt from pin PB_4 
+	attachInterrupt_ADC_EOC(adc_eoc_isr, 8);	// set the ISR to be called for adc EOC interrupt
 	
 	enableADC();
 	
+	setupChannel_CTU_trigger(0); 	// link the PIT to the ADC channel 0 (PB4) through the CTU
+	
+	setupChannelPIT(3, 100);  // use PIT_3, the only one to be linked to the CTU. period =100ms.
+	startChannelPIT(3);	
 }
 
 void adc_watchdog_example()
