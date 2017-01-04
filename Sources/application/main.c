c1e5f480978705a0279305aa2834956e72cd473e
Harras-elmehdi <harraselemdhi@gmail.com>
2017-01-04 11:25:40 +0100

    Le main

---
+++ Sources/application/main.c
@@ -0,0 +1,46 @@
+/************ Include *********************/
+#include "MPC5604B.h"
+#include "IntcInterrupts.h"
+#include "config.h"
+#include "SBC.h"
+#include "Mode_manager.h"
+#include "MPC5604B_LED.h"
+#include "BCM_appli.h"
+#include "DCM_appli.h"
+#include "pin.h"
+#include "adc.h"
+#include "define.h"
+#include "spi_drv.h"
+#include "pit.h"
+#include "Buttons_management.h"
+#include "mode_manager_new.h"
+
+/*************** Private function prototype **********/
+
+/*************** Public function            **********/
+
+
+ int main(void) {
+	 
+	disableWatchdog();
+	initModesAndClock();
+	initialise_SPI_DRIVER();
+	SPI[1].init(SPI_BAUD_62500, SPI_DELAY_DEFAULT);
+	Init_SBC_DBG();
+	
+}
+	 
+				
+
+
+
+
+
+ 
+	
+	
+	
+
+
+
+
