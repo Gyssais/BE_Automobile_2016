569320fe091aa07f0890f39289345dcca9315ff5
The00 <leurent@etud.insa-toulouse.fr>
2017-01-11 08:57:58 +0100

    implemente MC33887 driver

--- Project_Headers/MC33887.h
+++ Project_Headers/MC33887.h
@@ -11,4 +11,21 @@
 #include "define.h"
 #include "MPC5604B.h"
 
+#define SENS1		0
+#define SENS2		1
+
+struct MC33887_pinout 
+{
+	uint8_t IN1;
+	uint8_t IN2;
+	uint8_t D2;
+	uint8_t EN;
+	uint8_t FS;
+};
+typedef struct MC33887_pinout MC33887_pinout;
+
+void init_HBridge(const MC33887_pinout * pinout);
+void stop_HBridge(const MC33887_pinout * pinout);
+void start_HBridge(const MC33887_pinout * pinout, int sens);
+
 #endif /* MC33887_H_ */
--- Sources/driver/MC33887.c
+++ Sources/driver/MC33887.c
@@ -7,3 +7,46 @@
 
 
 #include "MC33887.h"
+
+
+void init_HBridge(const MC33887_pinout * pinout)
+{
+
+	SIU.PCR[pinout->IN1].R = 0x0200;  // out
+	SIU.PCR[pinout->IN2].R = 0x0200;  // out
+	SIU.PCR[pinout->EN].R = 0x0200;	 // out
+	SIU.PCR[pinout->D2].R = 0x0200;   // out
+	SIU.PCR[pinout->FS].R = 0x0100;   // in
+
+	
+	SIU.GPDO[pinout->IN1].B.PDO = 0;
+	SIU.GPDO[pinout->IN2].B.PDO = 0;
+	SIU.GPDO[pinout->EN].B.PDO = 0;
+	SIU.GPDO[pinout->D2].B.PDO = 0;
+}
+
+
+void stop_HBridge(const MC33887_pinout * pinout)
+{
+	SIU.GPDO[pinout->IN1].B.PDO = 0;
+	SIU.GPDO[pinout->IN2].B.PDO = 0;
+	SIU.GPDO[pinout->D2].B.PDO = 0;
+	
+}
+
+void start_HBridge(const MC33887_pinout * pinout, int sens)
+{
+	SIU.GPDO[pinout->EN].B.PDO = 1;
+	SIU.GPDO[pinout->D2].B.PDO = 1;	
+	
+	if(sens == SENS1)
+	{
+		SIU.GPDO[pinout->IN1].B.PDO = 0;
+		SIU.GPDO[pinout->IN2].B.PDO = 1;
+	}
+	else if(sens == SENS2)
+	{
+		SIU.GPDO[pinout->IN1].B.PDO = 1;
+		SIU.GPDO[pinout->IN2].B.PDO = 0;
+	}
+}
